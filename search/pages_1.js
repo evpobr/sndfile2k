737ff665220e8f01780cf8a6929ad43f165b4ec0
evpobr <evpobr@gmail.com>
2018-03-10 17:15:06 +0500

    Bump v1.1.1

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['embedded_20sound_20files',['Embedded Sound Files',['../md_docs_embedded_files.html',1,'']]]
+  ['embedded_20sound_20files',['Embedded Sound Files',['../md_embedded_files.html',1,'']]]
 ];
