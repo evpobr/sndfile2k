7b5ae41884a1f50550d72b797f2308810a528311
evpobr <evpobr@gmail.com>
2018-04-05 10:28:51 +0500

    Remove gsm610 library its own config.h

--- src/GSM610/gsm_destroy.c
+++ src/GSM610/gsm_destroy.c
@@ -7,15 +7,7 @@
 #include "gsm.h"
 #include "config.h"
 
-#ifdef HAS_STDLIB_H
 #include <stdlib.h>
-#else
-#ifdef HAS_MALLOC_H
-#include <malloc.h>
-#else
-extern void free();
-#endif
-#endif
 
 void gsm_destroy(gsm S)
 {
